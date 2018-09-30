8c9ba7726dbd6cc90c6bf6dd2575ebff857bf167
Marlon Marques <marlonxm@hotmail.com>
2018-09-30 12:05:56 -0300

    recuperar a senha incompleto

--- vendor/hcodebr/php-classes/src/DB/Sql.php
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -53,17 +53,11 @@ class Sql {
 
 	public function select($rawQuery, $params = array()):array
 	{
-
 		$stmt = $this->conn->prepare($rawQuery);
-
 		$this->setParams($stmt, $params);
-
 		$stmt->execute();
-
 		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-
 	}
-
 }
 
  ?>
